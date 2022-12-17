51c1ba7a2ab8630a2172b84be8221a01f21df7e2
Akmal Ariq <109916703+akmalariq9@users.noreply.github.com>
2022-12-17 14:55:53 +0700

    controller-update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,15 +13,24 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.robot.Robot;
+import javafx.scene.control.Label;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
-import javafx.scene.text.Text;
+//import javafx.scene.text.Text;
+
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private Label congrats;
+
+    @FXML
+    private Label ScoreText;
 
     @FXML
     private AnchorPane scene;
@@ -39,16 +48,21 @@ public class Controller implements Initializable {
     private Button startButton;
 
     @FXML
-    private Text score;
+    private Label EnglishIndicatorLabel;
+
+    //@FXML
+    //private Text score;
 
     Robot robot = new Robot();
-    private int scoreCounter = 0;
+    //private int scoreCounter = 0;
     private ArrayList<Rectangle> bricks = new ArrayList<>();
 
     double deltaX = -1;
     double deltaY = -3;
+    int initscore = 0;
 
-//1 Frame evey 10 millis, which means 100 FPS
+
+    //1 Frame evey 10 millis, which means 100 FPS
     Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent actionEvent) {
@@ -61,13 +75,16 @@ public class Controller implements Initializable {
             if(!bricks.isEmpty()){
                 bricks.removeIf(brick -> checkCollisionBrick(brick));
             } else {
-                createBricks();
+                congrats.setText("Congratulations!");
+                timeline.stop();
+                startButton.setVisible(true);
             }
             checkCollisionScene(scene);
             checkCollisionBottomZone();
         }
     }));
-    
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         timeline.setCycleCount(Animation.INDEFINITE);
@@ -76,14 +93,17 @@ public class Controller implements Initializable {
     @FXML
     void startGameButtonAction(ActionEvent event) {
         startButton.setVisible(false);
+        congrats.setText("");
         startGame();
+        initscore = 0;
+        ScoreText.setText(String.format("%d", initscore));
     }
 
     public void startGame(){
         createBricks();
         timeline.play();
     }
-    
+
     public void checkCollisionScene(Node node){
         Bounds bounds = node.getBoundsInLocal();
         boolean rightBorder = circle.getLayoutX() >= (bounds.getMaxX() - circle.getRadius());
@@ -113,13 +133,12 @@ public class Controller implements Initializable {
                 deltaY *= -1;
             }
             scene.getChildren().remove(brick);
-            scoreCounter++;
-            //score.setText(String.valueOf(scoreCounter));
+            initscore += 1;
+            ScoreText.setText(String.format("%d", initscore));
             return true;
         }
         return false;
     }
-
 
     public void createBricks(){
         double width = 560;
@@ -127,8 +146,8 @@ public class Controller implements Initializable {
 
         int spaceCheck = 1;
 
-        for (double i = height; i > 0 ; i = i - 50) {
-            for (double j = width; j > 0 ; j = j - 25) {
+        for (double i = height; i > 0 ; i = i - 200) {
+            for (double j = width; j > 0 ; j = j - 500) {
                 if(spaceCheck % 2 == 0){
                     Rectangle rectangle = new Rectangle(j,i,30,25);
                     rectangle.setFill(Color.BLUEVIOLET);
@@ -139,7 +158,7 @@ public class Controller implements Initializable {
             }
         }
     }
-    
+
     public void movePaddle(){
         Bounds bounds = scene.localToScreen(scene.getBoundsInLocal());
         double sceneXPos = bounds.getMinX();
@@ -180,8 +199,8 @@ public class Controller implements Initializable {
             bricks.forEach(brick -> scene.getChildren().remove(brick));
             bricks.clear();
             startButton.setVisible(true);
-            scoreCounter = 0;
-            score.setText(String.valueOf(scoreCounter));
+            initscore = 0;
+            ScoreText.setText("");
 
             deltaX = -1;
             deltaY = -3;
@@ -189,7 +208,7 @@ public class Controller implements Initializable {
             circle.setLayoutX(300);
             circle.setLayoutY(300);
 
-            System.out.println("Game over!");
+            // System.out.println("Game over!");
         }
     }
 }
