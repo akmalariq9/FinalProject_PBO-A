c376e9e1976703a588206e4a1a398cf6423e758a
Dewangga Dika Darmawan <dewangga3d@gmail.com>
2022-12-17 17:02:41 +0700

    lifes-feature-update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,10 +13,11 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.robot.Robot;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
-import javafx.scene.text.Text;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -24,13 +25,25 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
+    private Label congrats;
+
+    @FXML
+    private Label ScoreText;
+
+    @FXML
+    private Label Lifes;
+
+    @FXML
+    private Rectangle paddle;
+
+    @FXML
     private AnchorPane scene;
 
     @FXML
     private Circle circle;
 
-    @FXML
-    private Rectangle paddle;
+    //@FXML
+    //private Rectangle paddle;
 
     @FXML
     private Rectangle bottomZone;
@@ -39,21 +52,25 @@ public class Controller implements Initializable {
     private Button startButton;
 
     @FXML
-    private Text score;
+    private Label EnglishIndicatorLabel;
+
+    //@FXML
+    //private Text score;
 
     Robot robot = new Robot();
-    private int scoreCounter = 0;
+    //private int scoreCounter = 0;
     private ArrayList<Rectangle> bricks = new ArrayList<>();
 
     double deltaX = -1;
     double deltaY = -3;
+    int initscore = 0;
+    int initlifes = 3;
 
-//1 Frame evey 10 millis, which means 100 FPS
+    //1 Frame evey 10 millis, which means 100 FPS
     Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent actionEvent) {
             movePaddle();
-
             checkCollisionPaddle(paddle);
             circle.setLayoutX(circle.getLayoutX() + deltaX);
             circle.setLayoutY(circle.getLayoutY() + deltaY);
@@ -61,13 +78,26 @@ public class Controller implements Initializable {
             if(!bricks.isEmpty()){
                 bricks.removeIf(brick -> checkCollisionBrick(brick));
             } else {
-                createBricks();
+                congrats.setText("Congratulations!");
+                timeline.stop();
+                startButton.setVisible(true);
             }
             checkCollisionScene(scene);
-            checkCollisionBottomZone();
+            if(circle.getBoundsInParent().intersects(bottomZone.getBoundsInParent()))
+            {
+                if(initlifes == 0){
+                    checkCollisionBottomZone();
+                }
+                else{
+                    initlifes -= 1;
+                    Lifes.setText(String.format("%d", initlifes));
+                    circle.setLayoutX(300);
+                    circle.setLayoutY(27);
+                }
+            }
         }
     }));
-    
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         timeline.setCycleCount(Animation.INDEFINITE);
@@ -76,14 +106,19 @@ public class Controller implements Initializable {
     @FXML
     void startGameButtonAction(ActionEvent event) {
         startButton.setVisible(false);
+        congrats.setText("");
         startGame();
+        initscore = 0;
+        initlifes = 3;
+        ScoreText.setText(String.format("%d", initscore));
+        Lifes.setText(String.format("%d", initlifes));
     }
 
     public void startGame(){
         createBricks();
         timeline.play();
     }
-    
+
     public void checkCollisionScene(Node node){
         Bounds bounds = node.getBoundsInLocal();
         boolean rightBorder = circle.getLayoutX() >= (bounds.getMaxX() - circle.getRadius());
@@ -113,13 +148,12 @@ public class Controller implements Initializable {
                 deltaY *= -1;
             }
             scene.getChildren().remove(brick);
-            scoreCounter++;
-            //score.setText(String.valueOf(scoreCounter));
+            initscore += 1;
+            ScoreText.setText(String.format("%d", initscore));
             return true;
         }
         return false;
     }
-
 
     public void createBricks(){
         double width = 560;
@@ -127,11 +161,11 @@ public class Controller implements Initializable {
 
         int spaceCheck = 1;
 
-        for (double i = height; i > 0 ; i = i - 50) {
-            for (double j = width; j > 0 ; j = j - 25) {
+        for (double i = height; i > 0 ; i = i - 500) {
+            for (double j = width; j > 0 ; j = j - 150) {
                 if(spaceCheck % 2 == 0){
-                    Rectangle rectangle = new Rectangle(j,i,30,25);
-                    rectangle.setFill(Color.BLUEVIOLET);
+                    Rectangle rectangle = new Rectangle(j,i,30,30);
+                    rectangle.setFill(Color.ORANGE);
                     scene.getChildren().add(rectangle);
                     bricks.add(rectangle);
                 }
@@ -139,7 +173,7 @@ public class Controller implements Initializable {
             }
         }
     }
-    
+
     public void movePaddle(){
         Bounds bounds = scene.localToScreen(scene.getBoundsInLocal());
         double sceneXPos = bounds.getMinX();
@@ -156,14 +190,14 @@ public class Controller implements Initializable {
         }
     }
 
-    public void checkCollisionPaddle(Rectangle paddle){
+    public void checkCollisionPaddle(Rectangle paddle2){
 
-        if(circle.getBoundsInParent().intersects(paddle.getBoundsInParent())){
+        if(circle.getBoundsInParent().intersects(paddle2.getBoundsInParent())){
 
-            boolean rightBorder = circle.getLayoutX() >= ((paddle.getLayoutX() + paddle.getWidth()) - circle.getRadius());
-            boolean leftBorder = circle.getLayoutX() <= (paddle.getLayoutX() + circle.getRadius());
-            boolean bottomBorder = circle.getLayoutY() >= ((paddle.getLayoutY() + paddle.getHeight()) - circle.getRadius());
-            boolean topBorder = circle.getLayoutY() <= (paddle.getLayoutY() + circle.getRadius());
+            boolean rightBorder = circle.getLayoutX() >= ((paddle2.getLayoutX() + paddle.getWidth()) - circle.getRadius());
+            boolean leftBorder = circle.getLayoutX() <= (paddle2.getLayoutX() + circle.getRadius());
+            boolean bottomBorder = circle.getLayoutY() >= ((paddle2.getLayoutY() + paddle.getHeight()) - circle.getRadius());
+            boolean topBorder = circle.getLayoutY() <= (paddle2.getLayoutY() + circle.getRadius());
 
             if (rightBorder || leftBorder) {
                 deltaX *= -1;
@@ -180,16 +214,14 @@ public class Controller implements Initializable {
             bricks.forEach(brick -> scene.getChildren().remove(brick));
             bricks.clear();
             startButton.setVisible(true);
-            scoreCounter = 0;
-            score.setText(String.valueOf(scoreCounter));
-
+            initscore = 0;
+            ScoreText.setText("");
+            Lifes.setText("");
             deltaX = -1;
             deltaY = -3;
-
             circle.setLayoutX(300);
             circle.setLayoutY(300);
-
-            System.out.println("Game over!");
+            // System.out.println("Game over!");
         }
     }
 }
