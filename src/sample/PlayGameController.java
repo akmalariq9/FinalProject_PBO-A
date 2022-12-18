ae886dee2912808b95583ac5e076bda81a858772
Dewangga Dika Darmawan <dewangga3d@gmail.com>
2022-12-18 12:38:53 +0700

    update

--- src/sample/PlayGameController.java
+++ src/sample/PlayGameController.java
@@ -1,8 +1,11 @@
 package sample;
 
 import javafx.animation.Animation;
+import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
+import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -14,6 +17,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.robot.Robot;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
@@ -21,6 +25,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.scene.text.Text;
+import javafx.scene.transform.Rotate;
 
 public class PlayGameController implements Initializable {
 
@@ -28,7 +33,7 @@ public class PlayGameController implements Initializable {
     private Label AfterPlayText;
 
     @FXML
-    private Text AreYouReady;
+    private ImageView AreYouReady;
 
     @FXML
     private Button StartButton2;
@@ -43,7 +48,7 @@ public class PlayGameController implements Initializable {
     private Label Lifes;
 
     @FXML
-    private Rectangle paddle;
+    private ImageView paddle;
 
     @FXML
     private AnchorPane scene;
@@ -51,8 +56,11 @@ public class PlayGameController implements Initializable {
     @FXML
     private Circle circle;
 
-    //@FXML
-    //private Rectangle paddle;
+    @FXML
+    private ImageView red_amogus;
+
+    @FXML
+    private ImageView cyan_amogus;
 
     @FXML
     private Rectangle bottomZone;
@@ -63,17 +71,16 @@ public class PlayGameController implements Initializable {
     @FXML
     private Label EnglishIndicatorLabel;
 
-    //@FXML
-    //private Text score;
-
     Robot robot = new Robot();
-    //private int scoreCounter = 0;
     private ArrayList<Rectangle> bricks = new ArrayList<>();
+    private ImageView[] playerLifes;
 
-    double deltaX = -1;
-    double deltaY = -3;
+    private String urlLife = "/resources/heart.png";
+
+    double deltaX = 1;
+    double deltaY = 3;
     int initscore = 0;
-    int initlifes = 3;
+    int initlifes = 2;
 
     //1 Frame evey 10 millis, which means 100 FPS
     Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
@@ -94,15 +101,9 @@ public class PlayGameController implements Initializable {
             checkCollisionScene(scene);
             if(circle.getBoundsInParent().intersects(bottomZone.getBoundsInParent()))
             {
-                if(initlifes == 0){
-                    checkCollisionBottomZone();
-                }
-                else{
-                    initlifes -= 1;
-                    Lifes.setText(String.format("%d", initlifes));
-                    circle.setLayoutX(300);
-                    circle.setLayoutY(27);
-                }
+                removeLife();
+                circle.setLayoutX(450);
+                circle.setLayoutY(45);
             }
         }
     }));
@@ -110,6 +111,8 @@ public class PlayGameController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         timeline.setCycleCount(Animation.INDEFINITE);
+        amogusMoveRotate(cyan_amogus);
+        amogusMoveRotate(red_amogus);
     }
 
     @FXML
@@ -117,11 +120,15 @@ public class PlayGameController implements Initializable {
         startButton.setVisible(false);
         StartButton2.setVisible(false);
         AreYouReady.setVisible(false);
+        createGameElements(urlLife);
+        amogusMoveTranslation(red_amogus, 10000, 850); // red = 1000 ; cyan = 2000
+        amogusMoveRotate(red_amogus);
+        amogusMoveTranslation(cyan_amogus, 10000, -850);
+        amogusMoveRotate(cyan_amogus);
         startGame();
         initscore = 0;
-        initlifes = 3;
+        initlifes = 2;
         ScoreText.setText(String.format("%d", initscore));
-        Lifes.setText(String.format("%d", initlifes));
     }
 
     public void startGame(){
@@ -160,7 +167,6 @@ public class PlayGameController implements Initializable {
             scene.getChildren().remove(brick);
             initscore += 50;
             ScoreText.setText(String.format("%d", initscore));
-            Lifes.setText(String.format("%d", initlifes));
             return true;
         }
         return false;
@@ -190,7 +196,7 @@ public class PlayGameController implements Initializable {
         double sceneXPos = bounds.getMinX();
 
         double xPos = robot.getMouseX();
-        double paddleWidth = paddle.getWidth();
+        double paddleWidth = paddle.getFitWidth();
 
         if(xPos >= sceneXPos + (paddleWidth/2) && xPos <= (sceneXPos + scene.getWidth()) - (paddleWidth/2)){
             paddle.setLayoutX(xPos - sceneXPos - (paddleWidth/2));
@@ -201,13 +207,13 @@ public class PlayGameController implements Initializable {
         }
     }
 
-    public void checkCollisionPaddle(Rectangle paddle2){
+    public void checkCollisionPaddle(ImageView paddle2){
 
         if(circle.getBoundsInParent().intersects(paddle2.getBoundsInParent())){
 
-            boolean rightBorder = circle.getLayoutX() >= ((paddle2.getLayoutX() + paddle.getWidth()) - circle.getRadius());
+            boolean rightBorder = circle.getLayoutX() >= ((paddle2.getLayoutX() + paddle.getFitWidth()) - circle.getRadius());
             boolean leftBorder = circle.getLayoutX() <= (paddle2.getLayoutX() + circle.getRadius());
-            boolean bottomBorder = circle.getLayoutY() >= ((paddle2.getLayoutY() + paddle.getHeight()) - circle.getRadius());
+            boolean bottomBorder = circle.getLayoutY() >= ((paddle2.getLayoutY() + paddle.getFitHeight()) - circle.getRadius());
             boolean topBorder = circle.getLayoutY() <= (paddle2.getLayoutY() + circle.getRadius());
 
             if (rightBorder || leftBorder) {
@@ -241,10 +247,69 @@ public class PlayGameController implements Initializable {
     
     @FXML
     void StartButton2Action(ActionEvent event) {
-        initlifes = 3;
+        initlifes = 2;
+        createGameElements(urlLife);
         startGame();
         StartButton2.setVisible(false);
         AfterPlayText.setText("");
         SumScore.setText("");
+    }
+
+    private void createGameElements(String urlLife) {
+		initlifes = 2;
+		playerLifes = new ImageView[3];
+		
+		for(int i = 0; i < playerLifes.length; i++) {
+			playerLifes[i] = new ImageView(urlLife);
+            playerLifes[i].setScaleX(0.025);
+            playerLifes[i].setScaleY(0.025);
+			playerLifes[i].setLayoutX(-955 + (i * 60));
+			playerLifes[i].setLayoutY(-655);
+			scene.getChildren().add(playerLifes[i]);	
+		}
+    }
+    
+    private void removeLife() {
+		scene.getChildren().remove(playerLifes[initlifes]);
+		initlifes -= 1;
+		if(initlifes < 0) {
+			timeline.stop();
+            AfterPlayText.setText("Game Over :(");
+            SumScore.setText(String.format("Your Score : %d", initscore));
+            bricks.forEach(brick -> scene.getChildren().remove(brick));
+            bricks.clear();
+            StartButton2.setVisible(true);
+
+            initscore = 0;
+            ScoreText.setText("");
+            Lifes.setText("");
+            deltaX = -1;
+            deltaY = -3;
+            circle.setLayoutX(300);
+            circle.setLayoutY(300);
+		}
+	}
+
+    public void amogusMoveTranslation(ImageView amogus, double speed, double x) {
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(amogus);
+        translate.setDuration(Duration.millis(speed));
+        translate.setCycleCount(TranslateTransition.INDEFINITE);
+        translate.setByX(x);
+        translate.setByY(-150);
+        translate.setAutoReverse(true);
+        translate.play();
+    }
+
+
+    public void amogusMoveRotate(ImageView amogus) {
+        RotateTransition rotate = new RotateTransition();
+        rotate.setNode(amogus);
+        rotate.setDuration(Duration.millis(5000));
+        rotate.setCycleCount(TranslateTransition.INDEFINITE);
+        rotate.setInterpolator(Interpolator.LINEAR);
+        rotate.setByAngle(360);
+        rotate.setAxis(Rotate.Z_AXIS);
+        rotate.play();
     }
 }
